7b04f797f0e7e9c29b8b58eb8ca36e234b8daea1
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-02-01 20:19:28 -0500

    Modify determineSubscriptionFeeForUsers to get ready for calculating proRate fee

--- subs/sub.ts
+++ subs/sub.ts
@@ -18,13 +18,22 @@ export interface ISub {
 export const charges = (yearMonth: string, subscription: ISub, users: IUser[]): number => {
     const subscribers = users
         .filter(user => filterUsersInMonthAndSubscription(user, yearMonth, subscription));
+
     return (subscribers.length > 1) ? determineSubscriptionFeeForUsers(subscribers, subscription) : 0;
 }
 
 type determineSubs = (users: IUser[], subscription: ISub) => number;
 
-const determineSubscriptionFeeForUsers: determineSubs = (billableUsers, subscription: ISub) =>
-    (billableUsers.length * subscription.price);
+const determineSubscriptionFeeForUsers: determineSubs = (billableUsers, subscription: ISub) => {
+
+    return billableUsers.reduce((interval, user) => {
+        if(!user.deactive) {
+            return subscription.price + interval;
+        }
+        // we need to pro-rate
+        return interval; //@TODO: this is temporary
+    }, 0)
+};
 
 
 
