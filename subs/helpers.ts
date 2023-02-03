4936dc528c6df430d3c289651a5d9a180316f2e6
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-02-02 20:54:08 -0500

    Fix the month range determination.

--- subs/helpers.ts
+++ subs/helpers.ts
@@ -2,16 +2,19 @@ import { ISub, IUser } from "./sub";
 
 type filterUsers = (user: IUser, yearMonth: string, subscription: ISub) => boolean;
 
-export const filterUsersInMonthAndSubscription: filterUsers = (user, yearMonth, subscription) => (isDateWithinMonth(yearMonth, user.active))
-    && user.customerId === subscription.cusId;
+export const filterUsersInMonthAndSubscription: filterUsers = (user, yearMonth, subscription) => (user.customerId === subscription.cusId)
+    && (isDateWithinMonth(yearMonth, user.active));
 
 type isDateInRange = (yearMonth: string, userDate: Date) => boolean;
 export const isDateWithinMonth: isDateInRange = (yearMonth, userDate) => {
-    const targetDate = lastDay(new Date(`${yearMonth}-05`));
+    const targetDate = firstDay(new Date(`${yearMonth}`));
 
-    let minimumDate = firstDay(userDate)
-    let maximumDate = lastDay(userDate);
+    const subYear = userDate.getUTCFullYear();
+    const subMonth = getCorrectMonthOrdinal(userDate.getUTCMonth());
 
+    let minimumDate = firstDay(new Date(`${subYear}-${subMonth}`))
+    let maximumDate = lastDay(new Date(`${subYear}-${subMonth}`))
+    
     if (targetDate >= minimumDate
         && targetDate <= maximumDate) {
         return true;
@@ -19,7 +22,9 @@ export const isDateWithinMonth: isDateInRange = (yearMonth, userDate) => {
     return false;
 }
 
+const getCorrectMonthOrdinal = (month: number) =>  month === 0 ? '01' : month === 11 ? '12' : month;
+
 type dateGetters = (date: Date) => Date;
-export const firstDay: dateGetters = (date) => new Date(date.getFullYear(), date.getMonth(), 1);
-export const lastDay: dateGetters = (date) => new Date(date.getFullYear(), date.getMonth() + 1, 0);
+export const firstDay: dateGetters = (date) => new Date(date.getFullYear(), date.getMonth() + 1);
+export const lastDay: dateGetters = (date) => new Date(date.getFullYear(), date.getMonth() + 2, 0);
 export const next: dateGetters = (date) => new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1);
